0031683f0cf053263e260cfac7ef54d8d04970e6
jackexe123 <sawyercampbell777@outlook.com>
2021-12-21 12:50:22 -0500

    Dee.

---
+++ source/InLovingMemory.hx
@@ -0,0 +1,44 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+import flixel.input.keyboard.FlxKeyboard;
+import flixel.input.keyboard.FlxKey;
+
+class CreditsState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+	
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('Dee', 'shared'));
+		bg.screenCenter();
+		add(bg);
+
+		FlxG.sound.music.fadeIn(4, 0, 0.7);
+	}
+
+	override function update(elapsed:Float)
+	{
+
+		if (controls.ACCEPT)
+		{
+            fancyOpenURL("https://www.suicidestop.com/call_a_hotline.html");
+		}
+
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new TitleState());
+		}
+		super.update(elapsed);
+	}
+}
